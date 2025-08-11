279a514af646c4c8bf05812bfdac9a8b5e274f1e
sumeka <sumeka.arasakumar@aspiresys.com>
2025-08-11 10:39:00 +0530

    Added logout button o n the navigation bar

--- tab/src/components/Home.jsx
+++ tab/src/components/Home.jsx
@@ -3,6 +3,7 @@ import "../App.css";
 import {NavLink, Outlet} from "react-router-dom";
 import "../css/HomePage.css";
 import Footer from "./Footer";
+import LogoutButton from "./LogoutButton";
 
 
 function Home() {
@@ -11,29 +12,29 @@ function Home() {
   <div className="tab-content">
      
         
-
-    <div>
    
-       <nav className="tab-nav">
-         <div className="nav-header">SHOPPIFY</div>
+    <nav className="tab-nav">
+      
+      <div className="nav-header">SHOPPIFY</div>
+          <div className="tab-nav-links">
+
+       <div className="tab-left">                                  
         <NavLink to="/home/about" className="tab-link">About</NavLink>
         <NavLink to="/home/newlaunch" className="tab-link">NewLaunch</NavLink>
         <NavLink to="/home/profile" className="tab-link">Profile</NavLink>
         <NavLink to="/home/products" className={({ isActive }) => isActive ? "tab-link active" : "tab-link"}>Products</NavLink>
-         <NavLink to="/home/contacts" className="tab-link">Contacts</NavLink> 
+        <NavLink to="/home/contacts" className="tab-link">Contacts</NavLink> 
         <NavLink to="/home/test" className="tab-link">Test</NavLink>
-        
-       
 
-      </nav>
+         </div>
+       <div className="tab-right">
+        <LogoutButton/>
+       </div>
+</div>
+    </nav>
 
-      <div>
-        <Outlet />
-      </div>
-      
-
-     </div>
-       <Footer/>
+      <div className="main-content"> <Outlet/>  </div>
+      <Footer/>
   </div>
   )}
 
