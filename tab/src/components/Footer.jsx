cdb3a9c344f523f24b1ee996b56f37910127098a
sumeka <sumeka.arasakumar@aspiresys.com>
2025-08-25 10:35:57 +0530

    Footer - text changes Package.json -  installed recat icons

--- tab/src/components/Footer.jsx
+++ tab/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer-container">
   <footer className='footer'>
-            © {new Date().getFullYear()} - Shoppify. All rights reserved.
+          CopyRight  © {new Date().getFullYear()} - Shoppify | All rights reserved.
         </footer>
     </div>
   )
