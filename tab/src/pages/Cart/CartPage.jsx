6cf97c708b976f8afe48e5973b7abb67ddb3828e
sumeka <sumeka.arasakumar@aspiresys.com>
2025-09-01 12:26:02 +0530

    cart page

--- tab/src/pages/Cart/CartPage.jsx
+++ tab/src/pages/Cart/CartPage.jsx
@@ -3,9 +3,10 @@ import { CartContext } from '../../context/CartContext'
 import { ToastContainer,toast } from 'react-toastify';
 
 const CartPage = () => {
-    const {cart, removeFromCart, clearCart }= useContext(CartContext);
+    const {cart, removeFromCart, clearCart, increaseQty, decreaseQty}= useContext(CartContext);
 
     const totalAmount = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
     if( cart.length === 0) return<h2> Your cart is Empty. </h2>
 
     const handleCheckout=()=>{
@@ -14,29 +15,33 @@ const CartPage = () => {
 
   return (
    <div>
-     <h2>Shopping Cart!!</h2>
+     <h2>Shopping Cart -  Items Added !</h2>
      <div className='cart-container'>
         {cart.map((item)=> (
-        //    <div key={item.id} style={{ marginBottom: "10px" }}>
-                <div key={item.id} className='cart-item'>
-
-                {/* <img src={item.image} alt={item.title} width="50" /> */}
-                <img src={item.image} alt={item.title}/>
-               <span>Item:{item.title}</span>
-                <span>{item.quantity}</span>
-                <span>Price(₹): {item.price * item.quantity}</span>
-                <a onClick={()=> removeFromCart(item.id)}>Remove</a>
+           <div key={item.id} className='cart-item'>
+               <img src={item.image} alt={item.title}/>
+               <p>{item.title}</p><br/>
+               <p style={{fontWeight:'bold'}}>(₹) : {item.price * item.quantity}</p>
+               
+                <div className='cart-quantity'>
+                    <button onClick={()=> decreaseQty(item.id)}>-</button>
+                    <span>{item.quantity}</span>
+                    {/* p - block element(quantity is not visible because of block level), span - inline element */}
+                    <button onClick={()=> increaseQty(item.id)}>+</button>
+                </div>   
+                <a onClick={()=> removeFromCart(item.id)} className='remove-Btn'>Remove</a>
                 {/* <button onClick={()=> removeFromCart(item.id)}>Remove</button> */}
             </div>
         ))
         }
-        {/* <h3 className='totalAmount'>Total:  ₹{totalAmount.toFixed(2)}</h3> */}
-      
     </div>
-      <h3>Total:  ₹{totalAmount.toFixed(2)}</h3>
+          <br/>
+        <h3>Total Items  -  {totalItems} </h3>
+        <h3 className='total-Amount'>Total :  {totalAmount.toFixed(2)} ₹</h3>
+          <br/> 
         <button onClick={clearCart} style={{marginRight:"12px"}}>Clear Cart</button>
         <button onClick={handleCheckout}>Proceed to Checkout</button>
-         <ToastContainer position='top-center' autoClose={3000}/>
+         <ToastContainer position='top-center' autoClose={3000}/><br/><br/><br/>
 
    </div>
   )
