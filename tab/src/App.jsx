b07358d337aaebf983ba6e7e0966fde4112cdb2d
sumeka <sumeka.arasakumar@aspiresys.com>
2025-09-15 12:24:33 +0530

    App.css, index.jsx - changes in common alignment App.jsx -  added components in routes

--- tab/src/App.jsx
+++ tab/src/App.jsx
@@ -20,18 +20,27 @@ import ProtectedRoutes from "./utils/ProtectedRoutes";
 import CartPage from "./pages/Cart/CartPage";
 import { CartProvider } from "./context/CartContext";
 import { ToastContainer } from "react-toastify";
+import Electronics from "./pages/Products/Electronics";
+import Jewellery from "./pages/Products/Jewellery";
+import Books from "./pages/Products/Books";
+import Clothing from "./pages/Products/Clothing";
+import { WishlistProvider } from "./context/WishlistContext";
+import WishlistPage from "./pages/Wishlist/WishlistPage";
+import PaymentPage from "./pages/Cart/PaymentPage";
+import CartSliderContent from "./pages/Cart/CartSliderContent";
 
 
 function App() {
   return (
     <CartProvider>
+      <WishlistProvider>
       <Router>
-      <Routes>
+       <Routes>
         {/* <Route path="/" element={<Navigate to="/home/about" />} /> */}
     
         {/* <Route path="/home" element={<HomePage />}>
           <Route path="about" element={<About />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={<Profile >} />
           <Route path="products" element={<Products />} />
           <Route path="contacts" element={<Contacts />} />
           <Route path="test" element={<Test />} />
@@ -40,6 +49,8 @@ function App() {
           <Route path="/" element={<HomePage/>}/>
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="login" element={<Login/>}/>
+          <Route path="wishlist" element={<WishlistPage/>}/>
+
           
           
           <Route path="/home" element={
@@ -58,12 +69,24 @@ function App() {
           <Route path="contacts" element={<Contacts />} />
           <Route path="test" element={<Test />} />
           <Route path="cart" element={<CartPage/>}/>
+          <Route path="electronics" element={<Electronics/>}/>
+          <Route path="jewellery" element={<Jewellery/>}/>
+          <Route path="books" element={<Books/>}/>
+          <Route path="clothing" element={<Clothing/>}/>
+          <Route path="wishlist" element={<WishlistPage/>}/>
+          <Route path="cartSlider" element={<CartSliderContent/>}/>
+          <Route path="payment" element={<PaymentPage/>}/>
+
+
+
         </Route>
       </Routes>
 
       <ToastContainer position="top-right" autoClose={3000} />
 
     </Router>
+      </WishlistProvider>
+      
     </CartProvider>
   );
 }
