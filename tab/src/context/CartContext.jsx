0b41a0831410afbe954ba15a3456c7693b5716d2
sumeka <sumeka.arasakumar@aspiresys.com>
2025-09-18 11:08:14 +0530

    Cart - changes logic in decrement(after 1 is decrement, have to remove from cart) using filter to remove the item , in which quantity is > 0.

--- tab/src/context/CartContext.jsx
+++ tab/src/context/CartContext.jsx
@@ -44,8 +44,9 @@ export const CartProvider=({children})=>{
     const decreaseQty =(id)=>{ 
         setCart((prev) =>
              prev.map((item)=>
-                   item.id === id && item.quantity >1 ? {...item, quantity: item.quantity -1} :  item
+                   item.id === id ? {...item, quantity: item.quantity -1} :  item
             )
+            .filter((item) =>item.quantity >0) //here it removes item with 0
         )
     }
 
